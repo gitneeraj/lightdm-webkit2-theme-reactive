a5ae17b18a1a3499b0679734b2351b9dec8de90a
gitneeraj <36226187+gitneeraj@users.noreply.github.com>
2021-05-14 16:33:01 +0530

    Feature/power (#1)
    * 0.1.4
    
    * add power options working

---
+++ src/components/base/Cancel.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Link } from 'svelte-routing'
+  import Icon from './Icon.svelte'
+</script>
+
+<Link to="/" class="link-icon">
+  <Icon name="arrow-left" class="cancel" width="22" height="22" />
+  <span>Cancel</span>
+</Link>
