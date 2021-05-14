2d40d460485995cda4635083dae676e8509fad00
Neeraj <neeraj.a4353@gmail.com>
2021-05-14 16:59:31 +0530

    show selected dm

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,14 @@
 <script>
-  import { Link } from 'svelte-routing'
-  import Icon from './base/Icon.svelte'
+  // import { Link } from 'svelte-routing'
+  // import Icon from './base/Icon.svelte'
+  import DateTime from './base/datetime.svelte'
 </script>
 
 <header class="header-wrapper">
   <div id="settings">
-    <Link to="settings" class="link-icon">
+    <!-- <Link to="settings" class="link-icon">
       <Icon name="cog" class="cog" width="32" height="32" />
-    </Link>
+    </Link> -->
   </div>
-  <div id="system-info">system info</div>
+  <div id="system-info"><DateTime /></div>
 </header>
---
+++ src/components/base/datetime.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from 'svelte'
+
+  const months = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
+  }
+
+  let date = new Date()
+  $: hour = date.getHours()
+  $: min = date.getMinutes()
+  $: sec = date.getSeconds()
+  $: month = date.getUTCMonth() + 1
+  $: day = date.getUTCDate()
+  $: year = date.getUTCFullYear()
+
+  onMount(() => {
+    setInterval(() => {
+      date = new Date()
+    }, 1000)
+  })
+</script>
+
+<div>
+  {day}
+  {months[month]}
+  {year}
+  {hour} : {min} : {sec <= 9 ? `0${sec}` : sec}
+</div>
