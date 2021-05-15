11e883968a53e3d34ee542e157a4b643318cc9ec
gitneeraj <36226187+gitneeraj@users.noreply.github.com>
2021-05-15 14:35:38 +0530

    Feature/ghpages (#4)
    * build script for gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
 
 export default ({ mode }) => {
-  const path =
-    mode === 'prod' ? '/usr/share/lightdm-webkit/themes/reactive/' : '/'
+  let path = '/'
+
+  if(mode === 'prod'){
+    path = '/usr/share/lightdm-webkit/themes/reactive/'
+  }else if (mode === 'ghpages'){
+    path = 'lightdm-webkit2-theme-reactive/'
+  }
 
   return defineConfig({
     plugins: [svelte()],
