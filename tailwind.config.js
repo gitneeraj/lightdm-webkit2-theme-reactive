0c1ec523e990640f44d90380dbe747e61eaafcf2
Neeraj <neeraj.a4353@gmail.com>
2021-05-22 16:06:59 +0530

    update to use tailwindcss-jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{svelte,js}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
