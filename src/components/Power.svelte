0661ea061f02c1589fad40bb41883a211c06a018
gitneeraj <36226187+gitneeraj@users.noreply.github.com>
2021-05-23 16:13:36 +0530

    Feature/cleanup (#10)
    * 0.4.0
    
    * updates and some cleanups
    
    * updates and some cleanups
    
    * cleanups

--- src/components/Power.svelte
+++ src/components/Power.svelte
@@ -18,21 +18,21 @@
 <div class="power-outer-wrapper flex flex-col items-center" in:fade>
   <div class="power-wrapper flex flex-col sm:flex-row mb-8">
     <div
-      class="w-48 text-3xl py-8 flex flex-col justify-center items-center text-gray-100 dark:text-gray-400 shadow-lg bg-opacity-20 backdrop-filter backdrop-blur-sm rounded-lg border-2 bg-white border-gray-50 border-opacity-10 border-solid dark:bg-gray-700 dark:bg-opacity-60 mr-8 cursor-pointer hover:bg-opacity-10 dark:hover:bg-opacity-20 transition duration-200"
+      class="w-32 h-32 sm:w-48 sm:h-48 text-lg sm:text-3xl py-8 flex flex-col justify-center items-center text-gray-100 dark:text-gray-400 shadow-lg bg-opacity-20 backdrop-filter backdrop-blur-sm rounded-lg border-2 bg-white border-gray-50 border-opacity-10 border-solid dark:bg-gray-700 dark:bg-opacity-60 mb-6 sm:mb-0 mr-0 sm:mr-8 cursor-pointer hover:bg-opacity-10 dark:hover:bg-opacity-20 transition duration-200"
       on:click={handleOnShutDown}
     >
       <Icon name="power" class="power mb-3" width="48" height="48" />
       <span class="font-family-light">Shutdown</span>
     </div>
     <div
-      class="w-48 text-3xl py-8 flex flex-col items-center justify-center text-gray-100 dark:text-gray-400 shadow-lg bg-opacity-20 backdrop-filter backdrop-blur-sm rounded-lg border-2 bg-white border-gray-50 border-opacity-10 border-solid dark:bg-gray-700 dark:bg-opacity-60 mr-8 cursor-pointer hover:bg-opacity-10 dark:hover:bg-opacity-20 transition duration-200"
+      class="w-32 h-32 sm:w-48 sm:h-48 text-lg sm:text-3xl py-8 flex flex-col items-center justify-center text-gray-100 dark:text-gray-400 shadow-lg bg-opacity-20 backdrop-filter backdrop-blur-sm rounded-lg border-2 bg-white border-gray-50 border-opacity-10 border-solid dark:bg-gray-700 dark:bg-opacity-60 mb-6 sm:mb-0 mr-0 sm:mr-8 cursor-pointer hover:bg-opacity-10 dark:hover:bg-opacity-20 transition duration-200"
       on:click={handleOnRestart}
     >
       <Icon name="refresh-2" class="refresh-2 mb-3" width="48" height="48" />
       <span class="font-family-light">Restart</span>
     </div>
     <div
-      class="w-48 text-3xl py-8 flex flex-col items-center justify-center text-gray-100 dark:text-gray-400 shadow-lg bg-opacity-20 backdrop-filter backdrop-blur-sm rounded-lg border-2 bg-white border-gray-50 border-opacity-10 border-solid dark:bg-gray-700 dark:bg-opacity-60 cursor-pointer hover:bg-opacity-10 dark:hover:bg-opacity-20 transition duration-200"
+      class="w-32 h-32 sm:w-48 sm:h-48 text-lg sm:text-3xl py-8 flex flex-col items-center justify-center text-gray-100 dark:text-gray-400 shadow-lg bg-opacity-20 backdrop-filter backdrop-blur-sm rounded-lg border-2 bg-white border-gray-50 border-opacity-10 border-solid dark:bg-gray-700 dark:bg-opacity-60 mb-6 sm:mb-0 cursor-pointer hover:bg-opacity-10 dark:hover:bg-opacity-20 transition duration-200"
       on:click={handleOnSuspend}
     >
       <Icon name="sleep" class="sleep mb-3" width="48" height="48" />
