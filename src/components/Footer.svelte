0212e3c25ddff48d7200793edc2b2995f606d527
gitneeraj <36226187+gitneeraj@users.noreply.github.com>
2021-05-27 15:36:39 +0530

    Feature/tailwind (#12)
    * adding tailwind css
    
    * updating colors and adding dark mode for login
    
    * adding theme
    
    * fix build and remove advanced css properties
    
    * add new styles for power page
    
    * power page small screen
    
    * remove Tailwindcss component
    
    * refactor styles
    
    * delete index.scss

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -16,13 +16,13 @@
 
 <footer class={`footer-wrapper ${$$props.class}`}>
   <div class="dm-list">
-    <Link to="dmlist" class="flex items-center text-gray-100 dark:text-gray-400">
+    <Link to="dmlist" class="icon-link">
       <Icon name="laptop" class="dm-laptop mr-2" width="32" height="32" />
       <span>{defaultDE}</span>
     </Link>
   </div>
   <div class="power-options">
-    <Link to="power" class="flex items-center text-gray-100 dark:text-gray-400">
+    <Link to="power" class="icon-link">
       <Icon name="power" class="power" width="32" height="32" />
     </Link>
   </div>
