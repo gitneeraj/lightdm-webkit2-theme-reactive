d447a7ed2ecddc1f8966cc14d0da6fff887a7238
Neeraj <neeraj.a4353@gmail.com>
2021-12-29 19:34:31 +0530

    show only 3 chars of month

--- src/components/base/datetime.svelte
+++ src/components/base/datetime.svelte
@@ -2,18 +2,18 @@
   import { onMount } from 'svelte'
 
   const months = {
-    0: 'January',
-    1: 'February',
-    2: 'March',
-    3: 'April',
+    0: 'Jan',
+    1: 'Feb',
+    2: 'Mar',
+    3: 'Apr',
     4: 'May',
-    5: 'June',
-    6: 'July',
-    7: 'August',
-    8: 'September',
-    9: 'October',
-    10: 'November',
-    11: 'December'
+    5: 'Jun',
+    6: 'Jul',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec'
   }
 
   let date = new Date()
